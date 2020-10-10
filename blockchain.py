954d407dee1ec7ed5730401e3463f5fc5a893dec
Mukesh Gurpude <55982424+Vpingdevil@users.noreply.github.com>
2020-10-10 20:28:07 +0530

    runtime enhancement
    removed file.write in loop as to reduce execution speed
    removed unnecessary variable
    used loop one-liners

--- blockchain.py
+++ blockchain.py
@@ -52,15 +52,14 @@ class Blockchain(object):
         return len(self.chain) > 0
 
     def create_genesis_block(self):
-        return Block(0, self.get_current_time(), "Thy Beginning", "0")
+        return Block(0, self.get_current_time(), "The Beginning", "0")
 
     def get_last_block(self):
         return self.chain[-1]
 
     def get_current_time(self):
         now = date.datetime.now()
-        time = "{0.day}/{0.month}/{0.year} {0.hour}:{0.minute}:{0.second}".format(now)
-        return time
+        return "{0.day}/{0.month}/{0.year} {0.hour}:{0.minute}:{0.second}".format(now)    # returning time, unnecessary variable removed
 
     def create_new_block(self):
         new_block_index = int(self.get_last_block().index) + 1
@@ -70,36 +69,33 @@ class Blockchain(object):
         self.chain.append(Block(new_block_index, new_block_timestamp, new_block_data, new_block_previous_hash))
 
     def is_chain_valid(self):
-        error = 0
+        error = False
         # Loop through chain and validate chain
         for j in range(1, len(self.chain)):
-            current_block = self.chain[j]
-            previous_block = self.chain[j - 1]
+            current_block, previous_block = self.chain[j], self.chain[j - 1]
 
             # First check if hash in current block is still the same
             # If not, then data in the block has been changed
             if current_block.hash != current_block.hash_block():
                 print("\n<Error:001> Block %d data was changed" % current_block.index)
-                error = 1
+                error = True
 
             # Then check if the current block and the previous block are still linked
             # i.e the current block's previous hash should equal the previous block's hash
             if current_block.previous_hash != previous_block.hash:
                 print("\n<Error:002> Block %d and Block %d are not linked. Different hash detected" %
                       (previous_block.index, current_block.index))
-                error = 1
+                error = True
 
-        if error == 0:
+        if not error:
             print("\n<Success> Block-chain is valid")
 
     def print_chain(self):
-        for block in self.chain:
-            print(block.print_block_info()),
+        print("\n".join([block.print_block_info() for block in self.chain]))
 
     def save_chain(self):
         with open("blockchain.txt", "w") as file:
-            for block in self.chain:
-                file.write(block.print_block_info())
+            file.write("\n".join([block.print_block_info() for block in self.chain]))
 
 # Create Blockchain
 my_chain = Blockchain()
